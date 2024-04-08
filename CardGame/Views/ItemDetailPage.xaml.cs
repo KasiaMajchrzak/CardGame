da9bd99aa17e4876ffb7fb63b5716385db54b27f
km_99 <katarzyna.majchrzak1999@wp.pl>
2024-04-08 18:23:01 +0200

    init

---
+++ CardGame/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CardGame.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CardGame.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
